9b2b4ce247970b177c3f2a4ec7cd67fb2c19c22b
Augusto Cesar Ferreira <gutocf@gmail.com>
2020-10-21 17:36:42 -0300

    Update tests for FilenameHandler

--- tests/TestCase/Lib/Filename/FilenameHandlerTest.php
+++ tests/TestCase/Lib/Filename/FilenameHandlerTest.php
@@ -8,8 +8,8 @@ use Cake\TestSuite\TestCase;
 class FilenameHandlerTest extends TestCase {
 
 	public function testSuffix() {
-		$filename = TEST_ROOT . 'tests' . DS . 'TestCase' . DS . 'Lib' . DS . 'Resizer' . DS . 'image_resizer_test.png';
-		$expected = TEST_ROOT . 'tests' . DS . 'TestCase' . DS . 'Lib' . DS . 'Resizer' . DS . 'image_resizer_test-0.png';
+		$filename = TEST_ROOT . 'tests' . DS . 'TestCase' . DS . 'Lib' . DS . 'Resizer' . DS . 'image_portrait.png';
+		$expected = TEST_ROOT . 'tests' . DS . 'TestCase' . DS . 'Lib' . DS . 'Resizer' . DS . 'image_portrait-0.png';
 		$actual = FilenameHandler::applyNumericSuffix($filename);
 		$this->assertEquals($expected, $actual);
 	}
