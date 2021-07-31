bc1c56858b517751659cea0018a17daad752c2a2
Augusto Cesar Ferreira <gutocf@gmail.com>
2021-07-31 19:50:18 -0300

    Adds Plugin.php file

---
+++ src/Plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gutocf\ImageUpload;
+
+use Cake\Console\CommandCollection;
+use Cake\Core\BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    protected $name = 'ImageUpload';
+
+    protected $bootstrapEnabled = false;
+
+    protected $routesEnabled = false;
+}
